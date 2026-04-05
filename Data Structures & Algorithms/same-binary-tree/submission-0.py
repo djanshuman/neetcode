26b8b05c1d40ad973fb6fb2a127d4329da7b6242
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-04-05 14:16:51 -0700

    Update submission-0.py

--- Data Structures & Algorithms/same-binary-tree/submission-0.py
+++ Data Structures & Algorithms/same-binary-tree/submission-0.py
@@ -1,3 +1,5 @@
+# BFS Iterative
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -11,9 +13,10 @@ class Solution:
 
         while queue:
             p_node, q_node = queue.popleft()
-            
+
             if p_node is None and q_node is None:
                 continue
+
             if p_node is None or q_node is None or (p_node.val != q_node.val):
                 return False
 
@@ -21,3 +24,51 @@ class Solution:
             queue.append((p_node.right,q_node.right))
 
         return True
+
+
+#DFS Iterative
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+e
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        stack = deque([(p,q)])
+
+        while stack:
+            p_node, q_node = stack.pop()
+            
+            if p_node is None and q_node is None:
+                continue
+            if p_node is None or q_node is None or (p_node.val != q_node.val):
+                return False
+
+            stack.append((p_node.left,q_node.left))
+            stack.append((p_node.right,q_node.right))
+
+        return True
+      
+      
+#DFS Recursive  
+        
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+e
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if not p or not q or p.val != q.val:
+            return False
+        
+        return (self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right))
