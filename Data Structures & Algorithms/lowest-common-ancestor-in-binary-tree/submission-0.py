acafeef6b13a927aad69cf74c8bdb5cd8a9618e5
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-04-08 20:41:13 -0700

    Update submission-0.py

--- Data Structures & Algorithms/lowest-common-ancestor-in-binary-tree/submission-0.py
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-tree/submission-0.py
@@ -21,9 +21,11 @@ class Solution:
             left = checkLowestCommonAncestor(node.left)
             right = checkLowestCommonAncestor(node.right)
 
+            ''' In case p and q are same then threshold will be 1 , if we don't have this check then it will never reach 2 and it will return false '''
             mid = node == p or node == q
+            threshold = 1 if p == q else 2
 
-            if mid + left + right >= 2:
+            if mid + left + right >= threshold:
                 self.ans = node
 
             return mid or left or right
