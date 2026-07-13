60be9f494f2dc4871a51922ff0958adf4058a6de
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-07-12 20:50:36 -0700

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,26 @@
+import heapq
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        #each task takes n unit of time
+        # minimize idle time
+        # Frequency hashMap , heap + queue combination
+        # frequency in maxHeap, schedule on cooldown queue to hold tasks on cooldown and pop based on time match
+        count = {}
+        time = 0
+        q = deque() # [-cnt, t]
+        for i in tasks:
+            count[i] = count.get(i,0) + 1
+        maxHeap = [- val for key, val in count.items()]
+        heapq.heapify(maxHeap)
+        
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                val = 1 + heapq.heappop(maxHeap)
+                if val:
+                    q.append([val,time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
+
+
